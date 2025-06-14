f59aaf1bd50f1e98f700befd7632168a2fbd7ae8
qooba <dev@qooba.net>
2025-06-14 02:47:29 +0200

    add j2 templates

--- tweaktune-python/tests/test_basic.py
+++ tweaktune-python/tests/test_basic.py
@@ -33,7 +33,7 @@ def test_basic_j2(request, output_dir, j2_file):
         .with_workers(1)\
         .with_j2_template("output", j2_file)\
     .iter_range(number)\
-        .add_column("value",lambda data: "world")\
+        .add_column("value", lambda data: "world")\
         .write_jsonl(path=output_file, template="output")\
     .run()
 
