7014b1dc74157b93a2263a7a6f70752967586f63
qooba <dev@qooba.net>
2025-07-13 15:24:02 +0200

    change ui

--- tweaktune-python/tweaktune/__init__.py
+++ tweaktune-python/tweaktune/__init__.py
@@ -10,18 +10,18 @@ import inspect
 from typing import Dict, List, Union, Tuple, Callable, Optional
 from pydantic import BaseModel
 
-def graph_item(name: str):
+def step_item(name: str):
     frame = inspect.currentframe().f_back
     args_info = {str(k):str(v) for k,v in inspect.getargvalues(frame).locals.items() if k != 'self'}
-    return GraphItem(name=name, func=frame.f_code.co_name, args=args_info)
+    return StepItem(name=name, func=frame.f_code.co_name, args=args_info)
 
-class GraphItem(BaseModel):
+class StepItem(BaseModel):
     name: str
     func: str
     args: Dict[str, str] = {}
-    children: List['GraphItem'] = []
+    children: List['StepItem'] = []
 
-    def add_child(self, child: 'GraphItem'):
+    def add_child(self, child: 'StepItem'):
         self.children.append(child)
 
 def config_item(name: str):
@@ -34,74 +34,89 @@ class ConfigItem(BaseModel):
     func: str
     args: Dict[str, str] = {}
 
+def start_item(name: str):
+    frame = inspect.currentframe().f_back
+    args_info = {str(k):str(v).replace('"','') for k,v in inspect.getargvalues(frame).locals.items() if k != 'ipc_data' and k != 'self'}
+    return ConfigItem(name=name, func=frame.f_code.co_name, args=args_info)
+    
+class StartItem(BaseModel):
+    name: str
+    func: str
+    args: Dict[str, str] = {}
+
+class Graph(BaseModel):
+    config: List[ConfigItem] = []
+    steps: List[StepItem] = []
+    start: Optional[StartItem] = None
+
 
 
 class Pipeline:
     def __init__(self):
         self.builder = PipelineBuilder()
-        self.config = []
+        self.graph = Graph()
 
     def with_openapi_dataset(self, name: str, path_or_url: str):
         """Adds an OpenAPI dataset to the pipeline."""
         self.builder.with_openapi_dataset(name, path_or_url)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_tools_dataset(self, name: str, tools: List[callable]):
         """Converts a list of functions to json schema and adds them to the pipeline."""
         json_list = [function_to_json_schema(tool) for tool in tools]
         self.builder.with_json_list_dataset(name, json_list, None)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_pydantic_models_dataset(self, name: str, models: List[BaseModel]):
         """Converts a list of Pydantic models to json schema and adds them to the pipeline."""
         json_list = [pydantic_to_json_schema(model) for model in models]
         self.builder.with_json_list_dataset(name, json_list, None)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_dicts_dataset(self, name: str, dicts: List[dict], sql: str = None):
         """Converts a list of dictionaries to json schema and adds them to the pipeline."""
         json_list = [json.dumps(d) for d in dicts]
         self.builder.with_json_list_dataset(name, json_list, sql)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_jsonl_dataset(self, name: str, path: str, sql: str = None):
         """Adds a jsonl dataset to the pipeline."""
         self.builder.with_jsonl_dataset(name, path, sql)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_json_dataset(self, name: str, path: str, sql: str = None):
         """Adds a json dataset to the pipeline."""
         self.builder.with_json_dataset(name, path, sql)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_mixed_dataset(self, name: str, datasets: List[str]):
         """Adds a mixed dataset to the pipeline."""
         self.builder.with_mixed_dataset(name, datasets)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_polars_dataset(self, name: str, path: str, sql: str):
         """Adds a polars dataset to the pipeline."""
         self.builder.with_polars_dataset(name, path, sql)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_parquet_dataset(self, name: str, path: str, sql: str = None):
         """Adds a parquet dataset to the pipeline."""
         self.builder.with_parquet_dataset(name, path, sql)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_csv_dataset(self, name: str, path: str, delimiter: str, has_header: bool, sql: str = None):
         """Adds a csv dataset to the pipeline."""
         self.builder.with_csv_dataset(name, path, delimiter, has_header, sql)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_db_dataset(self, name: str, conn: str, query: str):
@@ -117,7 +132,7 @@ class Pipeline:
             table  = cx.read_sql(conn, query, return_type="arrow")
             ipc_data = record_batches_to_ipc_bytes(table.to_reader())
             self.builder.with_ipc_dataset(name, ipc_data)
-            self.config.append(config_item(name))
+            self.graph.config.append(config_item(name))
             return self
         except ModuleNotFoundError:
             package_installation_hint("connectorx")
@@ -129,7 +144,7 @@ class Pipeline:
             dataset = load_dataset(dataset_path, name=dataset_name, split=dataset_split)
             ipc_data = record_batches_to_ipc_bytes(dataset.data.to_reader())
             self.builder.with_ipc_dataset(name, ipc_data, sql)
-            self.config.append(config_item(name))
+            self.graph.config.append(config_item(name))
             return self
         except ModuleNotFoundError:
             package_installation_hint("datasets")
@@ -150,7 +165,7 @@ class Pipeline:
             else:
                 raise ValueError("Invalid dataset type")
             
-            self.config.append(config_item(name))
+            self.graph.config.append(config_item(name))
             return self
 
         except ModuleNotFoundError:
@@ -161,28 +176,28 @@ class Pipeline:
     def with_template(self, name: str, template: str):
         """Adds a template to the pipeline."""
         self.builder.with_jinja_template(name, template)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_j2_template(self, name: str, path: str, op_config: Optional[dict] = None):
         """Adds a template from file to the pipeline."""
         op_config = json.dumps(op_config, ensure_ascii=False) if op_config else None
         self.builder.with_j2_template(name, path, op_config)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_j2_templates(self, path: str, op_config: Optional[dict] = None):
         """Adds a template from file to the pipeline."""
         op_config = json.dumps(op_config, ensure_ascii=False) if op_config else None
         self.builder.with_j2_templates(path, op_config)
-        self.config.append(config_item("J2-TEMPLATES"))
+        self.graph.config.append(config_item("J2-TEMPLATES"))
         return self
     
     def with_llm(self, llm: LLM):
         """Adds a LLM to the pipeline."""
         if llm.__class__ == LLM.OpenAI:
             self.builder.with_llm_api(llm.name, llm.base_url, llm.api_key, llm.model, llm.max_tokens)
-            self.config.append(config_item(llm.name))
+            self.graph.config.append(config_item(llm.name))
         else:
             raise ValueError("Invalid LLM type")
         
@@ -191,19 +206,19 @@ class Pipeline:
     def with_llm_api(self, name: str, base_url: str, api_key: str, model: str, max_tokens: int = 2048, temperature: float = 0.7):
         """Adds an OpenAI LLM to the pipeline."""
         self.builder.with_llm_api(name, base_url, api_key, model, max_tokens, temperature)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_llm_openai(self, name: str, api_key: str, model: str, max_tokens: int = 2048, temperature: float = 0.7):
         """Adds an OpenAI LLM to the pipeline."""
         self.builder.with_llm_openai(name, api_key, model, max_tokens, temperature)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
 
     def with_llm_azure_openai(self, name: str, api_key: str, endpoint: str, deployment_name: str, api_version: str, max_tokens: int = 2048, temperature: float = 0.7):
         """Adds an OpenAI LLM to the pipeline."""
         self.builder.with_llm_azure_openai(name, api_key, endpoint, deployment_name, api_version, max_tokens, temperature)
-        self.config.append(config_item(name))
+        self.graph.config.append(config_item(name))
         return self
     
     def with_llm_mistralrs(self, name: str, 
@@ -217,7 +232,7 @@ class Pipeline:
                 in_situ_quant=in_situ_quant,
             )
             self.builder.with_llm_mistralrs(name, MistralrsWrapper(runner))
-            self.config.append(config_item(name))
+            self.graph.config.append(config_item(name))
             return self
         except ModuleNotFoundError:
             package_installation_hint("mistralrs")
@@ -257,7 +272,7 @@ class Pipeline:
             )
             FastLanguageModel.for_inference(model)
             self.builder.with_llm_unsloth(name, UnslothWrapper(model, tokenizer))
-            self.config.append(config_item(name))
+            self.graph.config.append(config_item(name))
 
             return self
         except ModuleNotFoundError:
@@ -267,7 +282,7 @@ class Pipeline:
     def with_embedings(self, embeddings: Embeddings):
         if embeddings.__class__ == Embeddings.OpenAI:
             self.builder.with_embeddings_api(embeddings.name, embeddings.model, embeddings.base_url, embeddings.api_key)
-            self.config.append(config_item("EMBEDDINGS"))
+            self.graph.config.append(config_item("EMBEDDINGS"))
         else:
             raise ValueError("Invalid Embeddings type")
         
@@ -275,7 +290,7 @@ class Pipeline:
     
     def with_workers(self, workers: int):
         self.builder.with_workers(workers)
-        self.config.append(config_item("WORKERS"))
+        self.graph.config.append(config_item("WORKERS"))
         return self
     
     def from_yaml(self, path_or_url: str):
@@ -285,10 +300,10 @@ class Pipeline:
     def iter(self, iter_by: IterBy):
         if iter_by.__class__ == IterBy.Range:
             self.builder.iter_by_range(iter_by.start, iter_by.stop, iter_by.step)
-            self.config.append(config_item("ITER-RANGE"))
+            self.graph.start = start_item("ITER-RANGE")
         elif iter_by.__class__ == IterBy.Dataset:
             self.builder.iter_by_dataset(iter_by.name)
-            self.config.append(config_item("ITER-DATASET"))
+            self.graph.start = start_item("ITER-DATASET")
         else:
             raise ValueError("Invalid IterBy type")
         
@@ -296,7 +311,7 @@ class Pipeline:
     
     def iter_dataset(self, name: str):
         self.builder.iter_by_dataset(name)
-        self.config.append(config_item("ITER-DATASET"))
+        self.graph.start = start_item("ITER-DATASET")
         return PipelineRunner(self.builder)
     
     def iter_range(self, *args, **kwargs):
@@ -315,24 +330,23 @@ class Pipeline:
             step = args[2]
 
         self.builder.iter_by_range(start, stop, step)
-        self.config.append(config_item("ITER-RANGE"))
-        return PipelineRunner(self.builder, self.config)
+        self.graph.start = start_item("ITER-RANGE")
+        return PipelineRunner(self.builder, self.graph)
 
 
 class PipelineRunner:
 
-    def __init__(self, builder: PipelineBuilder, config: List[ConfigItem] = None):
+    def __init__(self, builder: PipelineBuilder, graph: Graph = None):
         self.builder = builder
         self.step_index = 0
-        self.graph = []
-        self.config = config if config else []
+        self.graph = graph if graph else Graph()
 
     def __name(self, name: str):
         return f"{name}--{self.step_index}"
 
     def step(self, step, name: str = "PY-STEP"):
         self.builder.add_py_step(self.__name(name), PyStepWrapper(step))
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
@@ -340,7 +354,7 @@ class PipelineRunner:
         name = self.__name(name)
         step = type(name.replace("-","_"), (object,), {'check': lambda self, context: condition(context)})()
         self.builder.add_ifelse_step(name, PyConditionWrapper(step), then_chain.steps_chain, else_chain.steps_chain)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
@@ -348,7 +362,7 @@ class PipelineRunner:
         name = self.__name(name)
         step = type(name.replace("-","_"), (object,), {'process': lambda self, context: func(context)})()
         self.builder.add_py_step(name, PyStepWrapper(step))
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
@@ -360,7 +374,7 @@ class PipelineRunner:
             return context
 
         self.map(wrapper, name=name)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
@@ -371,7 +385,7 @@ class PipelineRunner:
             return context
 
         self.map(condition_wrapper, name=name)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
@@ -381,13 +395,13 @@ class PipelineRunner:
             return context
 
         self.map(wrapper, name=name)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
     def generate_text(self, template: str, llm: str, output: str, system_template: str = None, max_tokens: int = 1024, temperature: float = 0.1, name: str = "GENERATE-TEXT"):
         self.builder.add_text_generation_step(self.__name(name), template, llm, output, system_template, max_tokens, temperature)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
@@ -403,7 +417,7 @@ class PipelineRunner:
             schema = json.dumps(schema)
 
         self.builder.add_json_generation_step(self.__name(name), template, llm, output, json_path, system_template, schema, max_tokens, temperature, schema_template)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
@@ -412,54 +426,54 @@ class PipelineRunner:
     
     def sample(self, dataset: str, size: int, output: str, name: str = "SAMPLE"):
         self.builder.add_data_sampler_step(self.__name(name), dataset, size, output)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
     def render(self, template: str, output: str, name: str = "RENDER"):
         self.builder.add_render_step(self.__name(name), template, output)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
     def validate_json(self, schema: str, instance: str, name: str = "VALIDATE-JSON"):
         self.builder.add_validatejson_step(self.__name(name), schema, instance)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
     def chunk(self, capacity: Tuple[int, int], input: str, output: str, name: str = "CHUNK"):
         self.builder.add_chunk_step(self.__name(name), capacity, input, output)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
     def read(self, dataset: str, output: str, name: str = "SAMPLE"):
         self.builder.add_data_read_step(self.__name(name), dataset, output)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
     
     def judge(self, template: str, llm: str, name: str = "JUDGE"):
         self.builder.add_judge_step(self.__name(name), template, llm)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
     def validate(self, py_func, name: str = "VALIDATE"):
         self.builder.add_py_validator_step(self.__name(name), PyStepValidatorWrapper(py_func))
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         self.step_index += 1
         return self
 
     def write_jsonl(self, path: str, template: str, name: str = "WRITE-JSONL"):
         self.builder.add_write_jsonl_step(self.__name(name), path, template)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         return self
     
     def write_csv(self, path: str, columns: List[str], delimeter: str, name: str = "WRITE-JSONL"):
         self.builder.add_write_csv_step(self.__name(name), path, columns, delimeter)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         return self
 
     def print(self, *args, **kwargs):
@@ -470,7 +484,7 @@ class PipelineRunner:
             
         name = "PRINT"
         self.builder.add_print_step(self.__name(name), template=template, columns=columns)
-        self.graph.append(graph_item(name=self.__name(name)))
+        self.graph.steps.append(step_item(name=self.__name(name)))
         return self
 
     def debug(self, target: str = None):
@@ -490,7 +504,7 @@ class PipelineRunner:
         self.builder.compile()
         try:
             from tweaktune.ui import run_ui
-            run_ui(self.builder, self.graph, self.config, host, port)
+            run_ui(self.builder, self.graph, host, port)
         except ModuleNotFoundError:
             package_installation_hint("nicegui")
             raise
--- tweaktune-python/tweaktune/ui/__init__.py
+++ tweaktune-python/tweaktune/ui/__init__.py
@@ -8,26 +8,49 @@ config:
   layout: elk
   look: handDrawn
   theme: dark  
+  securityLevel: loose
 ---
 graph TD;\n"""
-    for ix,step in enumerate(graph):
+    callback_str = ""
+    for ix,config_step in enumerate(graph.config):
+        step_name = config_step.name
+        step_func = config_step.func
+        step_args = config_step.args
+        if "self" in step_args:
+            del step_args['self']
+
+        name = step_args.get("name") or step_args.get("workers") or ""
+
+        graph_str += f'  CONFIG{ix}["**{step_func}**<br/>{name}"] --> START[**{graph.start.func}**]\n'
+
+        callback_str += f'\n  click CONFIG{ix} call emitEvent("mermaid_click", "CONFIG@{ix}")'
+
+    callback_str += f'\n  click START call emitEvent("mermaid_click", "START@0")'
+    graph_str += "\n\n"
+
+    for ix,step in enumerate(graph.steps):
         step_name = step.name
         step_func = step.func
-        step_args = step.args
+        step_args = step.args.copy()
         if "self" in step_args:
             del step_args['self']
 
         if "name" in step_args:
-            del step_args['name']
+            del step_args["name"]
 
-        if ix == len(graph) - 1:
+        if ix == 0:
+            graph_str += f'  START[**{graph.start.func}**] --> STEP{ix}["**{step_func}**<br/>{step_args}"] -->'
+        elif ix == len(graph.steps) - 1:
             graph_str += f'  STEP{ix}["**{step_func}**<br/>{step_args}"]'
         else:
             graph_str += f'  STEP{ix}["**{step_func}**<br/>{step_args}"] -->'
 
+        callback_str += f'\n  click STEP{ix} call emitEvent("mermaid_click", "STEP@{ix}")'
+
+    graph_str = f"{graph_str}{callback_str}"
     return graph_str
 
-def run_ui(builder, graph, config, host: str="0.0.0.0", port: int=8080):
+def run_ui(builder, graph, host: str="0.0.0.0", port: int=8080):
 
     bus = queue.Queue()
 
@@ -74,6 +97,33 @@ def run_ui(builder, graph, config, host: str="0.0.0.0", port: int=8080):
         spinner.visible = False
         builder.stop()
 
+    with ui.dialog() as dialog_mermaid, ui.card():
+        mermaid = md = ui.markdown("")
+        #ui.button('Close', on_click=dialog_mermaid.close)
+
+    def run_mermaid_dialog(node):
+        node = node.split('@')
+        if node[0] == 'CONFIG':
+            node = int(node[1])
+            config_step = graph.config[node]
+            mermaid.content = f"**`{config_step.func}`**<br/>Args: `{config_step.args}`"
+        elif node[0] == 'STEP':
+            node = int(node[1])
+            step = graph.steps[node]
+            mermaid.content = f"**`{step.func}`**<br/>Args: `{step.args}`"
+        elif node[0] == 'START':
+            mermaid.content = f"**`{graph.start.func}`**<br/>Args: `{graph.start.args}`"
+            
+        
+        #mermaid.content = f"**TEST {node}**"
+
+        dialog_mermaid.open()
+        #ui.notify(f'Node {node} clicked !')
+
+    #ui.button('Graph', on_click=dialog_graph.open)
+
+    ui.on('mermaid_click', lambda e: run_mermaid_dialog(e.args))
+
     #with ui.footer().classes('bg-dark'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
 
     with ui.column().classes('h-10'):
@@ -82,9 +132,10 @@ def run_ui(builder, graph, config, host: str="0.0.0.0", port: int=8080):
     with ui.tabs().classes('w-full') as tabs:
         one = ui.tab('Graph')
         two = ui.tab('Logs')
-    with ui.tab_panels(tabs, value=two).classes('w-full'):
+    with ui.tab_panels(tabs, value=one).classes('w-full'):
         with ui.tab_panel(one).classes('flex flex-col items-center justify-center'):
-            ui.mermaid(render_graph(graph))
+            with ui.element('div').style('width: 80vw; height: 60vh; display: flex !important; justify-content: center !important; align-items: center !important;'):
+                ui.mermaid(render_graph(graph), config={'securityLevel': 'loose'} ).style('width: 50vw; height: 50vh; margin-left: 16vw; display: block;')
         with ui.tab_panel(two):
             log = ui.log(max_lines=10).classes('w-full h-200')
 
@@ -106,10 +157,6 @@ def run_ui(builder, graph, config, host: str="0.0.0.0", port: int=8080):
             #ui.notify(message)
 
     #ui.dark_mode().enable()
-    #with ui.dialog() as dialog_graph, ui.card():
-    #    ui.button('Close', on_click=dialog_graph.close)
-
-    #ui.button('Graph', on_click=dialog_graph.open)
 
 #    with ui.dialog() as dialog_config, ui.card():
 #        ui.button('Close', on_click=dialog_config.close)
