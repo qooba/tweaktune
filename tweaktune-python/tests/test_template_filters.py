37902b309789466f01d18e7bafa8bbb7338bea8a
qooba <dev@qooba.net>
2025-11-19 00:22:59 +0100

    add totoon filter

--- tweaktune-python/tests/test_template_filters.py
+++ tweaktune-python/tests/test_template_filters.py
@@ -190,7 +190,8 @@ def test_shuffle(request, output_dir, metadata):
     )
 
     lines = open(output_file).readlines()
-    assert set(json.loads(lines[0])) == set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    correct_set = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert set(json.loads(lines[0])) == set(correct_set)
 
 
 def test_tool_call(request, output_dir, metadata):
